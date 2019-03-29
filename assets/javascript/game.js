ca314cc29623adfb2329c61d583787b9125cfadc
Dr-Wyatt <daniel.ryuichi.wyatt@gmail.com>
2019-03-28 21:35:39 -0500

    Letters Populate for Word to guess, and win counter works

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,13 @@
 var wins = 0;
 var numberOfGuesses = 12;
 var lettersAlreadyGuessed = [];
-var lettersToGuess = []
-var wordsToGuess = ["Rhino", "Giraffe", "Dog", "Buffalo", "Cat"];
-var randomNumber = wordsToGuess[Math.floor(Math.random()*5)];
-for (var q=0; q < randomNumber.length; q++){
+var lettersToGuess = [];
+var wordsToGuess = ["rhino", "giraffe", "dog", "buffalo", "cat"];
+var randomWord = wordsToGuess[Math.floor(Math.random()*wordsToGuess.length)];
+for (var q=0; q < randomWord.length; q++){
     lettersToGuess[q] = "_";
 }
-var lettersToGuessNC = lettersToGuess.join(" ");
-var lettersLeft = randomNumber.length;
+var lettersLeft = randomWord.length;
 
 
 
@@ -17,12 +16,26 @@ var newline = "\r\n";
 
 document.onkeyup = function(event) {
     var player = event.key;
-
-    console.log(randomNumber);
+    console.log("player", player);
+    console.log(randomWord);
+    console.log(lettersToGuess);
           
     //correct & incorrect guess counter
     //use exec()?
     //update html
+
+        for (var c=0; c<randomWord.length; c++){
+            if (player === randomWord[c]){
+                lettersToGuess[c] = player;
+                console.log(player);
+                console.log(lettersToGuess);
+                // lettersleft--;
+                
+            }
+        }
+
+    
+    
     
     
     if (lettersAlreadyGuessed.indexOf(player) === -1){
@@ -39,15 +52,15 @@ document.onkeyup = function(event) {
     }
     //win counter
     
-    // if (lettersAlreadyGuessed = wordsToGuess[randomNumber]) {
-    //     wins++;
-    // }
+    if (lettersToGuess.join("") === randomWord) {
+        wins++;
+    }
     
 
     //html output
     var gameDiv = document.getElementById("game");
     gameDiv.style.whiteSpace = "pre";
-    gameDiv.textContent = " Wins: " + wins + newline + " Number of Guesses Left: " + numberOfGuesses + newline + " Letters Already Guessed: " + lettersAlreadyGuessed + newline + " Word to Guess: " + lettersToGuessNC; //+ display word to guess 
+    gameDiv.textContent = " Wins: " + wins + newline + " Number of Guesses Left: " + numberOfGuesses + newline + " Letters Already Guessed: " + lettersAlreadyGuessed + newline + " Word to Guess: " + lettersToGuess.join(""); //+ display word to guess 
 }
 
  
