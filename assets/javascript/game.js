0b77d3e3b3d1155e9717d1ac4eba00a38ffb1458
Dr-Wyatt <daniel.ryuichi.wyatt@gmail.com>
2019-03-29 19:03:35 -0500

    Words Display but RandomWord stays constant

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,64 +3,59 @@ var numberOfGuesses = 12;
 var lettersAlreadyGuessed = [];
 var lettersToGuess = [];
 var wordsToGuess = ["rhino", "giraffe", "dog", "buffalo", "cat"];
-var randomWord = wordsToGuess[Math.floor(Math.random()*wordsToGuess.length)];
-for (var q=0; q < randomWord.length; q++){
-    lettersToGuess[q] = "_";
-}
-var lettersLeft = randomWord.length;
-
-
-
 var newline = "\r\n";
 
 
-document.onkeyup = function(event) {
-    var player = event.key;
-    console.log("player", player);
-    console.log(randomWord);
-    console.log(lettersToGuess);
-          
-    //correct & incorrect guess counter
-    //use exec()?
-    //update html
+var randomWord = wordsToGuess[Math.floor(Math.random()*wordsToGuess.length)];
+for (var q=0; q < randomWord.length; q++){
+    lettersToGuess[q] = "_ ";
+}
 
-        for (var c=0; c<randomWord.length; c++){
+
+
+function game(){
+    
+    document.onkeyup = function() {
+        var player = event.key;
+        console.log(event);
+    
+        for (var c=0; c< randomWord.length; c++){
+
             if (player === randomWord[c]){
                 lettersToGuess[c] = player;
                 console.log(player);
                 console.log(lettersToGuess);
-                // lettersleft--;
+                if (lettersAlreadyGuessed.indexOf(player) === -1){
+                    lettersAlreadyGuessed.push(player);
+                    
+                    console.log(lettersAlreadyGuessed);
+                    
+                    numberOfGuesses--;
+                }
+                
                 
             }
+            if (lettersToGuess.join("") === randomWord) {
+                wins++;
+                lettersAlreadyGuessed.length = 0;
+                lettersToGuess.length = 0;
+                
+            }
+            else if (numberOfGuesses === 0){
+                
+            }
+            
         }
-
-    
-    
-    
-    
-    if (lettersAlreadyGuessed.indexOf(player) === -1){
-        lettersAlreadyGuessed.push(player);
         
-        console.log(lettersAlreadyGuessed);
-        numberOfGuesses--;
-    }
-    else if (lettersAlreadyGuessed.indexOf(player) === player){
-        numberOfGuesses++;
-    }
-    else {
-        numberOfGuesses--;
-    }
-    //win counter
-    
-    if (lettersToGuess.join("") === randomWord) {
-        wins++;
-    }
-    
 
-    //html output
-    var gameDiv = document.getElementById("game");
-    gameDiv.style.whiteSpace = "pre";
-    gameDiv.textContent = " Wins: " + wins + newline + " Number of Guesses Left: " + numberOfGuesses + newline + " Letters Already Guessed: " + lettersAlreadyGuessed + newline + " Word to Guess: " + lettersToGuess.join(""); //+ display word to guess 
-}
+        var gameDiv = document.getElementById("game");
+        gameDiv.style.whiteSpace = "pre";
+        gameDiv.textContent = " Wins: " + wins + newline + " Number of Guesses Left: " + numberOfGuesses + newline + " Letters Already Guessed: " + lettersAlreadyGuessed + newline + " Word to Guess: " + lettersToGuess.join("");
+    }
+    
+} 
+
+game();
+
 
  
